fbabb39bea305b4a25947f947959c150605bfe4e
craigjhamilton <37124410+craigjhamilton@users.noreply.github.com>
2018-05-10 18:08:26 +1200

    Install Confluent Platform

--- testscript.sh
+++ testscript.sh
@@ -1,1 +1,13 @@
-mkdir /home/craig/testfolder
+#Install Java
+sudo apt-get install default-jre 
+
+#Install Maven
+sudo apt install maven
+
+#Install Confluent Open Source Platform
+wget -qO - https://packages.confluent.io/deb/4.0/archive.key  | sudo apt-key add - 
+sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/4.0  stable main"
+sudo apt-get update && sudo apt-get install confluent-platform-oss-2.11 
+
+#Start the Confluent Platform
+confluent start
