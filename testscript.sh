24a4d363e294008127222ac7223b0ec4c76aafd3
craigjhamilton <37124410+craigjhamilton@users.noreply.github.com>
2018-05-11 10:31:04 +1200

    Add script to start connector

--- testscript.sh
+++ testscript.sh
@@ -1,4 +1,5 @@
-#Install Java
+#
+Install Java
 sudo apt-get install default-jre -y
 
 #Install Maven
@@ -32,8 +33,18 @@ echo "rest.port=8084"
 
 #Update the twitter connector properties file
 cd /home/craig/kafka-connect-twitter
+sudo cp twitter-source.properties.example twitter-source.properties
 sed -i '/topic=test/c\topic=twitter' twitter-source.properties
 sed -i '/twitter.consumerkey=/c\twitter.consumerkey=OyXUL6QBXa7e0Tq4oJrXQkZkl' twitter-source.properties
 sed -i '/twitter.consumersecret=/c\twitter.consumersecret=SxYRuZYBnxb9QUGgKTe7iPOKeP9MEaDjMyc2cYjIMgcysE6a0c' twitter-source.properties
 sed -i '/twitter.token=/c\twitter.token=717291305580126208-8u5eUBnXWmqMSrGpQ457UFNlmHCc3tG' twitter-source.properties
 sed -i '/twitter.secret=/c\twitter.secret=hmZfo0GbLdTfv1dHnPlSAQ1zpJOJ6mqCA2nSt2VozWpm2' twitter-source.properties
+sed -i '/# track.terms=news,music,hadoop,clojure,scala,fp,golang,python,fsharp,cpp,java/c\# track.terms=scala,databricks,java,kafka' twitter-source.properties
+sed -i '/# language=en,ru,de/c\# language=en' twitter-source.properties
+
+#Create the Kafka topic to write to 
+cd /usr/bin
+kafka-topics --zookeeper localhost:2181 --create --topic twitter --partitions 3 --replication- factor 1
+
+#Start the connector
+/usr/bin/connect-standalone /home/craig/kafka-connect-twitter/connect-source-standalone.properties /home/craig/kafka-connect-twitter/twitter-source.properties
