89a138a06c0d24497a28373a037c39d6dbfe2ccd
craigjhamilton <37124410+craigjhamilton@users.noreply.github.com>
2018-05-10 19:25:28 +1200

    Add GIT install command

--- testscript.sh
+++ testscript.sh
@@ -1,4 +1,3 @@
-
 #Install Java
 sudo apt-get install default-jre -y
 
@@ -8,12 +7,14 @@ sudo apt install maven -y
 #Install Confluent Open Source Platform
 wget -qO - https://packages.confluent.io/deb/4.0/archive.key  | sudo apt-key add - 
 sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/4.0  stable main"
-sudo apt-get update && sudo apt-get install confluent-platform-oss-2.11 -y
+sudo apt-get update 
+sudo apt-get install confluent-platform-oss-2.11 -y
 
 #Start the Confluent Platform
 confluent start
 
-#Clone GIT repo with Kafka Twitter Connector
+#Install GIT and Clone GIT repo with Kafka Twitter Connector
+sudo apt-get install git
 git clone https://github.com/Eneco/kafka-connect-twitter.git
 
 #Build the Jar files
