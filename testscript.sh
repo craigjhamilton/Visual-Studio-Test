5f10f6740ec3809a3e7b21922ce6b33c02ccf1d1
craigjhamilton <37124410+craigjhamilton@users.noreply.github.com>
2018-05-10 19:08:11 +1200

    Add commands to set up twitter connector

--- testscript.sh
+++ testscript.sh
@@ -1,3 +1,4 @@
+
 #Install Java
 sudo apt-get install default-jre -y
 
@@ -11,3 +12,27 @@ sudo apt-get update && sudo apt-get install confluent-platform-oss-2.11 -y
 
 #Start the Confluent Platform
 confluent start
+
+#Clone GIT repo with Kafka Twitter Connector
+git clone https://github.com/Eneco/kafka-connect-twitter.git
+
+#Build the Jar files
+cd /home/craig/kafka-connect-twitter
+mvn package
+
+#Update the Kafka config file to set the correct plugin path and rest API port
+sudo mkdir -p /usr/local/share/kafka/plugins/kafka-connect-twitter 
+sudo mv /home/craig/kafka-connect-twitter/target /usr/local/share/kafka/plugins/kafka-connect-twitter 
+
+echo "#Set the plugin path" >> /home/craig/kafka-connect-twitter/connect-source-standalone.properties 
+echo "plugin.path=/usr/local/share/kafka/plugins/kafka-connect-twitter" >> /home/craig/kafka-connect-twitter/connect-source-standalone.properties
+echo "#Set the REST API port" >> /home/craig/kafka-connect-twitter/connect-source-standalone.properties
+echo "rest.port=8084"
+
+#Update the twitter connector properties file
+cd /home/craig/kafka-connect-twitter
+sed -i '/topic=test/c\topic=twitter' twitter-source.properties
+sed -i '/twitter.consumerkey=/c\twitter.consumerkey=' twitter-source.properties
+sed -i '/twitter.consumersecret=/c\twitter.consumersecret=SxYRuZYBnxb9QUGgKTe7iPOKeP9MEaDjMyc2cYjIMgcysE6a0c' twitter-source.properties
+sed -i '/twitter.token=/c\twitter.token=717291305580126208-8u5eUBnXWmqMSrGpQ457UFNlmHCc3tG' twitter-source.properties
+sed -i '/twitter.secret=/c\twitter.secret=hmZfo0GbLdTfv1dHnPlSAQ1zpJOJ6mqCA2nSt2VozWpm2' twitter-source.properties
