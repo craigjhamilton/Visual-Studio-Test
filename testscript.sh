a4f527dacaca26958213c902b64128006dc8cdda
craigjhamilton <37124410+craigjhamilton@users.noreply.github.com>
2018-05-10 18:21:16 +1200

    Add -y options to install lines

--- testscript.sh
+++ testscript.sh
@@ -1,13 +1,13 @@
 #Install Java
-sudo apt-get install default-jre 
+sudo apt-get install default-jre -y
 
 #Install Maven
-sudo apt install maven
+sudo apt install maven -y
 
 #Install Confluent Open Source Platform
 wget -qO - https://packages.confluent.io/deb/4.0/archive.key  | sudo apt-key add - 
 sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/4.0  stable main"
-sudo apt-get update && sudo apt-get install confluent-platform-oss-2.11 
+sudo apt-get update && sudo apt-get install confluent-platform-oss-2.11 -y
 
 #Start the Confluent Platform
 confluent start
