672d1a6661bd64dac3c682529fb3ba125050678a
craigjhamilton <37124410+craigjhamilton@users.noreply.github.com>
2018-05-11 15:30:58 +1200

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -7,8 +7,7 @@ sudo apt install maven -y
 #Install Confluent Open Source Platform
 wget -qO - https://packages.confluent.io/deb/4.0/archive.key  | sudo apt-key add - 
 sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/4.0  stable main"
-sudo apt-get install confluent-platform-oss-2.11 -y
-sudo apt-get update 
+sudo apt-get install confluent-platform-oss-2.11 -y && sudo apt-get update 
 
 #Start the Confluent Platform
 confluent start
