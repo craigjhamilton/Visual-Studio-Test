b46ab4617a9170415a6a383667732add6b77a52f
craigjhamilton <37124410+craigjhamilton@users.noreply.github.com>
2018-05-22 18:06:17 +1200

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -5,9 +5,6 @@ sudo chmod -R 777 /tmp
 #Install Java
 sudo apt-get -y install default-jre 
 
-#Install Maven
-sudo apt-get -y install maven
-
 #Install Confluent Open Source Platform
 wget -qO - https://packages.confluent.io/deb/4.0/archive.key  | sudo apt-key add - 
 sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/4.0  stable main"
@@ -15,6 +12,9 @@ sudo apt-get -y update && sudo apt-get -y install confluent-platform-oss-2.11
 
 #Start the Confluent Platform
 confluent start
+
+#Install Maven
+sudo apt-get -y install maven
 
 #Install GIT and Clone GIT repo with Kafka Twitter Connector
 sudo apt-get -y install git
